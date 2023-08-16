44ca421349bd232a54c90309c2745b5acb1bd562
Isharab Ahmed <isharab.dev@gmail.com>
2023-08-16 22:48:34 +0500

    Flutter speech to text done

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:assist/Pages/feature_box.dart';
 import 'package:assist/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+// import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +14,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,105 +57,116 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         leading: const Icon(Icons.menu),
       ),
-      body: Column(
-        children: [
-          // virtual assistane picture part
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            // virtual assistane picture part
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 123,
                   decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
                     shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/virtualAssistant.png'),
+                    ),
                   ),
+                )
+              ],
+            ),
+            // chat bubble part
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin: const EdgeInsets.symmetric(
+                horizontal: 40,
+              ).copyWith(top: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: const Radius.circular(0),
+                ),
+                border: Border.all(
+                  color: Pallete.borderColor,
                 ),
               ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/virtualAssistant.png'),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  'Good Morning, What task should i assist you with?',
+                  style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 22,
                   ),
                 ),
-              )
-            ],
-          ),
-          // chat bubble part
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 40,
-            ).copyWith(top: 30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: const Radius.circular(0),
-              ),
-              border: Border.all(
-                color: Pallete.borderColor,
               ),
             ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'Good Morning, What task should i assist you with?',
+            // text field part
+            Container(
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(
+                left: 10,
+                top: 20,
+              ),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Here are a few suggestions',
                 style: TextStyle(
                   fontFamily: 'Cera Pro',
                   color: Pallete.mainFontColor,
-                  fontSize: 22,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          // text field part
-          Container(
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.only(
-              left: 10,
-              top: 20,
-            ),
-            alignment: Alignment.centerLeft,
-            child: const Text(
-              'Here are a few suggestions',
-              style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          // features list part
-          const Column(
-            children: [
-              FeatureBox(
-                color: Pallete.firstSuggestionBoxColor,
-                headerText: 'ChatGPT',
-                bodyText:
-                    'A smarter way to stay organized and informed with ChatGPT.',
-              ),
-              FeatureBox(
-                color: Pallete.secondSuggestionBoxColor,
-                headerText: 'DALL-E',
-                bodyText:
-                    'Get inspired and stay creative with your personal assistat powerd by Dall-E.',
-              ),
-              FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor,
-                headerText: 'Smart Voice Assistant',
-                bodyText:
-                    'Get the best of both worlds with voice assistant powered by Dall-E and ChatGPT.',
-              ),
-            ],
-          )
-        ],
+            // features list part
+            const Column(
+              children: [
+                FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: 'ChatGPT',
+                  bodyText:
+                      'A smarter way to stay organized and informed with ChatGPT.',
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: 'DALL-E',
+                  bodyText:
+                      'Get inspired and stay creative with your personal assistat powerd by Dall-E.',
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  bodyText:
+                      'Get the best of both worlds with voice assistant powered by Dall-E and ChatGPT.',
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: () {},
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            startListening();
+          } else if (speechToText.isListening) {
+            stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
         child: const Icon(Icons.mic),
       ),
     );
